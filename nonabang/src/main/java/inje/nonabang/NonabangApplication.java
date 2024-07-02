296f1c9700b292402bab7c17ae92df68b050a3e4
Gdm0714 <ehdals45454@naver.com>
2024-07-02 21:55:23 +0900

    :tada: init nonabang project

---
+++ nonabang/src/main/java/inje/nonabang/NonabangApplication.java
@@ -0,0 +1,13 @@
+package inje.nonabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonabangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonabangApplication.class, args);
+    }
+
+}
---
+++ nonabang/src/test/java/inje/nonabang/NonabangApplicationTests.java
@@ -0,0 +1,13 @@
+package inje.nonabang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonabangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
