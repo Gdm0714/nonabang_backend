472cb0dbd85328ff859d4e1350a3c4f939b42ee8
Jun3671 <wnsgur3027@naver.com>
2024-07-12 17:15:12 +0900

    :white_check_mark: 로그인 시 토근 반환하게 하여 인가하게 변경

--- src/main/java/inje/nonabang/controller/MemberController.java
+++ src/main/java/inje/nonabang/controller/MemberController.java
@@ -1,5 +1,7 @@
 package inje.nonabang.controller;
 
+import inje.nonabang.dto.ApiResponse;
+import inje.nonabang.dto.LoginRequest;
 import inje.nonabang.dto.MemberDTO;
 import inje.nonabang.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -9,19 +11,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MemberController {
 
-  private final MemberService memberService;
+    private final MemberService memberService;
 
 
     @GetMapping("/member/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
 
     @PostMapping("/member/save")
-    public void save(@RequestBody MemberDTO memberDTO){
+    public void save(@RequestBody MemberDTO memberDTO) {
         System.out.println("MemberController.save");
-        System.out.println("memberDTO= " + memberDTO );
+        System.out.println("memberDTO= " + memberDTO);
+
 
         memberService.save(memberDTO);
     }
+
+
+    @PostMapping("/member/login")
+    public ApiResponse login(@RequestBody LoginRequest memberDTO) {
+
+        return ApiResponse.success(memberService.login(memberDTO));
+
+    }
+
+
 }
