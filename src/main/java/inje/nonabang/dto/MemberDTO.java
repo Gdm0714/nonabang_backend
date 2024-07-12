c6b1c7862649493fe96818480c0b85d132e46c9a
Jun3671 <wnsgur3027@naver.com>
2024-07-12 17:22:35 +0900

    :white_check_mark: feat: 회원가입 구현

--- src/main/java/inje/nonabang/dto/MemberDTO.java
+++ src/main/java/inje/nonabang/dto/MemberDTO.java
@@ -1,15 +1,11 @@
 package inje.nonabang.dto;
 
 import inje.nonabang.entity.Member;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
+@Data
+@Builder
+
 public class MemberDTO {
     private String memberEmail;
     private String memberPassword;
@@ -19,13 +15,12 @@ public class MemberDTO {
 
     public static MemberDTO toMemberDTO(Member memberEntity) {
 
-        MemberDTO memberDTO = new MemberDTO();
-        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
-        memberDTO.setMemberName(memberEntity.getMemberName());
-        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
-        memberDTO.setMemberNumber(memberEntity.getMemberNumber());
-
-        return memberDTO;
+        return MemberDTO.builder()
+                .memberNumber(memberEntity.getMemberNumber())
+                .memberEmail(memberEntity.getMemberEmail())
+                .memberName(memberEntity.getMemberName())
+                .memberPassword(memberEntity.getMemberPassword())
+                .build();
     }
 
 }
--- src/main/java/inje/nonabang/service/MemberService.java
+++ src/main/java/inje/nonabang/service/MemberService.java
@@ -1,11 +1,15 @@
 package inje.nonabang.service;
 
 
+import inje.nonabang.dto.LoginRequest;
 import inje.nonabang.dto.MemberDTO;
 import inje.nonabang.entity.Member;
 import inje.nonabang.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // controller와 같이 final 멤버변수 생성자 만드는 역할
@@ -18,5 +22,26 @@ public class MemberService {
         //repository save 메서드 호출
         Member memberEntity = Member.toMemberEntity(memberDTO);
         memberRepository.save(memberEntity);
+    }
+
+    public MemberDTO login(LoginRequest memberDTO){
+        Optional<Member> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        System.out.println(byMemberEmail);
+        if(byMemberEmail.isPresent()){
+            Member member = byMemberEmail.get();
+            if(member.getMemberPassword().equals(memberDTO.getMemberPassword()))
+            {
+                MemberDTO dto = MemberDTO.toMemberDTO(member);
+                return dto;
+            }
+            else {
+                return null;
+            }
+        }
+        else
+        {
+            return null;
+        }
+
     }
 }
